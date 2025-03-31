aa073d77bd79454e9a0ebd4216c8c2ccdbf9aa2c
aman shaikh <aman@Mac.lan>
2025-03-31 18:15:40 +0530

    google analytics tags

--- frontend/app/layout.js
+++ frontend/app/layout.js
@@ -1,6 +1,6 @@
 import { Geist, Geist_Mono, Inter, Montserrat } from "next/font/google";
 import "./globals.css";
-import Script from "next/script";
+import { GoogleAnalytics } from "nextjs-google-analytics";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -32,24 +32,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        <Script
-          strategy="afterInteractive"
-          src={`https://www.googletagmanager.com/gtag/js?id=G-WV7W3J2PDW`}
-        />
-        <Script
-          id="google-analytics"
-          strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', 'G-WV7W3J2PDW', {
-                page_path: window.location.pathname,
-              });
-            `,
-          }}
-        />
+        <GoogleAnalytics gaId="G-WV7W3J2PDW" trackPageViews />
       </head>
       <body
         className={` font-montserrat ${geistSans.variable} ${geistMono.variable} ${montserrat.variable} ${inter.className} antialiased bg-white`}
